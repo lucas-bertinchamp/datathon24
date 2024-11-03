969117999aaea2f858a4d0f750303c5b484abf2b
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2024-11-03 17:25:02 -0500

    Summary generation part 2

--- summarization/summarize_text.py
+++ summarization/summarize_text.py
@@ -1,7 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
-from .pdf_extract import *
+from summarization.pdf_extract import *
 import json
+import time
 
 def create_prompt(text):
     prompt = f"Summarize all useful information for a financial analyst. Be exhaustive and concise by avoiding unnecessary words. NO SENTENCES. Do not introduce your answer" + "\n\n" + text
@@ -20,7 +21,7 @@ def call_model(prompt, client, model_id):
         response = client.converse(
             modelId=model_id,
             messages=conversation,
-            inferenceConfig={"maxTokens":1024,"temperature":0},
+            inferenceConfig={"maxTokens":2048,"temperature":0},
         )
 
         # Extract and print the response text.
@@ -35,27 +36,29 @@ def call_model(prompt, client, model_id):
 
 def summarize_text(text):
     
-    size_paragraph = 10000
+    size_paragraph = len(text)
     summary = ""
     
     for i in range(0, len(text), size_paragraph):
-        print(f"Processing paragraph {i//size_paragraph}")
+        print(f"Processing paragraph {i//size_paragraph + 1} / {len(text)//size_paragraph + 1} ...")
         paragraph = text[i:i+size_paragraph]
         user_message = create_prompt(paragraph)
         
         client = boto3.client("bedrock-runtime", region_name="us-west-2")
         
-        model_id = "meta.llama3-1-70b-instruct-v1:0"
+        model_id = "meta.llama3-1-405b-instruct-v1:0"
         
         response_text = call_model(user_message, client, model_id)
         
         summary += response_text
         
+        time.sleep(20)
+        
     return summary
 
 
 if __name__ == "__main__":
-    pdf_processed_path = "pdf\processed\processed_2020-annuel-10Q-FR-FINAL"
+    pdf_processed_path = "pdf/processed/processed_2020-annuel-10Q-FR-FINAL.txt"
     pdf_processed = ""
     pdf_filename_without_ext = os.path.splitext(os.path.basename(pdf_processed_path))[0]
     
