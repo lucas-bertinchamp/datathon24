61892dd472f2905db687e9f53f2754e8222874f2
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2024-11-03 20:04:13 -0500

    more info in summary

--- stmt_analysis/reddit_api.py
+++ stmt_analysis/reddit_api.py
@@ -16,19 +16,23 @@ def call_reddit_api(client_reddit, subreddits, company_name):
 
     all_posts = []
     
-    # Loop through each subreddit and fetch posts
-    for subreddit_name in subreddits:
-        subreddit = client_reddit.subreddit(subreddit_name)
-        
-        recent_posts = subreddit.search(f"title:{company_name}", sort='new', time_filter='month', limit=50)
-        for post in recent_posts:  # Fetch recent posts up to a month
-            if is_within_last_three_weeks(post.created_utc):
-                all_posts.append(post)
-        
-        popular_posts = subreddit.search(f"title:{company_name}", sort='top', time_filter='month', limit=50)
-        for post in popular_posts:  # Fetch recent popular posts up to a month
-            if is_within_last_three_weeks(post.created_utc):
-                all_posts.append(post)
+    try:
+        # Loop through each subreddit and fetch posts
+        for subreddit_name in subreddits:
+            subreddit = client_reddit.subreddit(subreddit_name)
+            
+            recent_posts = subreddit.search(f"title:{company_name}", sort='new', time_filter='month', limit=50)
+            for post in recent_posts:  # Fetch recent posts up to a month
+                if is_within_last_three_weeks(post.created_utc):
+                    all_posts.append(post)
+            
+            popular_posts = subreddit.search(f"title:{company_name}", sort='top', time_filter='month', limit=50)
+            for post in popular_posts:  # Fetch recent popular posts up to a month
+                if is_within_last_three_weeks(post.created_utc):
+                    all_posts.append(post)
+                    
+    except Exception as e:
+        return []
                 
     return all_posts
 
@@ -43,17 +47,4 @@ def format_post(post):
 
 if __name__ == "__main__":
     # Setup the Reddit connection
-    reddit = praw.Reddit(
-        client_id='zzAJ_bRHjE-f9dJh5ivO1w',
-        client_secret='JXTg1re8ADZYCkCFKFw48D-j8BTUyg',
-        user_agent='script:multi_subreddit_extractor (by u/Legal-Assistance6692)',
-        username='Legal-Assistance6692',
-        password='f54Uk92Z'
-    )
-
-    # Define the subreddits and company name you are searching for
-    subreddits = ['wallstreetbets', 'stocks', 'investing', 'options', 'pennystocks', 'SecurityAnalysis', 'ValueInvesting', 'DividendInvesting', 'Daytrading', 'algotrading', 'FinancialIndependence']
-    company_name = "Costco"  # Change this to the company you're interested in
-    all_posts = call_reddit_api(reddit, subreddits, company_name)
-    formatted_posts = [format_post(post) for post in all_posts]
-    print(formatted_posts)
+    pass
